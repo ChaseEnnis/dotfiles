da6e5b89d38c67401207075b204f8fe5c7e24fa4
Chase Ennis <ennis.18@wright.edu>
2024-03-18 13:07:45 -0400

    here

--- scripts/aws.sh
+++ scripts/aws.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 
-aws_install() {
+#!/bin/bash
 
-# Check if AWS CLI is already installed
+aws_install() {
+    # Check if AWS CLI is already installed
     if command -v aws &> /dev/null; then
         echo "[INFO] AWS CLI is already installed."
         return
     fi
-	sudo apt install unzip
-	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-	unzip awscliv2.zip
-	sudo ./aws/install
 
-
-
-
+    sudo apt install unzip -y  # Automatically answer yes to the prompt
+    yes | curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"  # Automatically answer yes to the prompt
+    unzip -o awscliv2.zip  # Overwrite existing files without prompting
+    sudo ./aws/install <<<"y"  # Automatically answer yes to the prompts
 }
 
 aws_install
+
